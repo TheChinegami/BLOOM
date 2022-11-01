bbb3b1151a851442f4ed3b8bb4eb5d4c8aa1fa6c
TheChinegami <simochang2002@gmail.com>
2022-11-01 16:22:11 +0100

    Initial commit

---
+++ src/Model/InvoiceRow.java
@@ -0,0 +1,54 @@
+package Model;
+
+public class InvoiceRow {
+    // attributes
+    private int invoiceId;
+    private int bagId;
+    private int quantity;
+
+    // constuctor
+    public InvoiceRow() {
+    }
+
+    public InvoiceRow(int invoiceId, int bagId, int quantity) {
+        this.invoiceId = invoiceId;
+        this.bagId = bagId;
+        this.quantity = quantity;
+    }
+
+    // getters
+    public int getInvoiceId() {
+        return invoiceId;
+    }
+
+    public int getBagId() {
+        return bagId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    // setters
+    public void setInvoiceId(int invoiceId) {
+        this.invoiceId = invoiceId;
+    }
+
+    public void setBagId(int bagId) {
+        this.bagId = bagId;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    // add new invoice row
+    public void add(InvoiceRow ir) {
+
+    }
+
+    // delete an invoice row
+    public void delete(int InvoiceId,int bagId) {
+
+    }
+}
